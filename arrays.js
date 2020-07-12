42022f5b72113edff61729d1de15bbb230b04265
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-07-12 21:07:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,5 +10,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
+
+   array.unshift(element);
+   return array;
 }
