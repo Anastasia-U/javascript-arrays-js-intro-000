82e85048323de3c0f9c0b7dbfd7bba141bbada19
anastasia Ukhtinskaya <anastassia94@gmail.com>
2020-07-12 21:01:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,8 @@ var chocolateBars = ["snickers",
                      "hundred grand",
                      "kitkat",
                      "sneakers" ];
+
+
+function addElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
